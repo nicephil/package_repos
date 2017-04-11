b9250f8eef7d0874ef3b9a50e35f3a4870baffce
Leilei Wang <leilei.wang@oakridge.io>
2017-04-11 16:45:01 +0800

    integrate jenkins

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-orign_file="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin"
+origin_file="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin"
+bin_file="bin_ap152_16M.app"
 
-tar czf ${orign_file}.tar.gz ${orign_file}
+tar czf ${origin_file}.tar.gz ${origin_file}
 
 # 64bytes header
 header="\x27\x05\x19\x56\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
 \x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
 \x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
 \x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30"
-echo -e "${header}" > bin_ap152_16m.app
-cat ${orign_file}.tar.gz >> bin_ap152_16m.app
+echo -e "${header}" > ${bin_file}
+cat ${origin_file}.tar.gz >> ${bin_file}
 
 [ -n "$1" ] && {
-    mv bin.app "$1"_bin_ap152_16m.app
+    mv ${bin_file} "$1"_${bin_file}
+    mv ${origin_file}.tar.gz "$1"_${origin_file}.tar.gz
 }
 
