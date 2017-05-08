900f547e057513eab698bb98887569e1ba5e2361
Leilei Wang <leilei.wang@oakridge.io>
2017-05-08 08:53:41 +0800

    move stainfo update from client event to sta response timer handler

--- ok_base-files/lib/okos/updatefields.sh
+++ ok_base-files/lib/okos/updatefields.sh
@@ -11,13 +11,13 @@ then
     _ip=`awk '{if ($4 == "'$mac'" && $6 == "'$vlan'") {print $1; exit}}' /proc/net/arp`
 fi
 
-_assoctime_rssi=`wlanconfig $ath list sta | awk '/'"$mac"'/{print $17"|"$6;exit}'`
-_assoctime=${_assoctime_rssi%%|*}
-_rssi=${_assoctime_rssi##*|}
+_chan_rssi_assoctime=`wlanconfig $ath list sta | awk '$1 ~ /'${mac}'/{print $3"'\'','\''"$6"'\'','\''"$17;exit}'`
+OIFS=$IFS;IFS='|';set -- $_chan_rssi_assoctime;_chan=$1;_rssi=$2;_assoctime=$3;IFS=$OIFS
 
+echo -e "\toption channel $_chan"
 echo -e "\toption assoctime $_assoctime"
 echo -e "\toption ipaddr $_ip"
 echo -e "\toption rssi $_rssi"
 
 #CREATE TABLE STAINFO(MAC,IFNAME,CHAN,RSSI,ASSOCTIME,RADIOID,BSSID,IPADDR,AUTHENTICATION,PORTAL_SCHEME,SSID,VLAN,PORTAL_MODE,PORTAL_USER);
-sqlite3 /tmp/stationinfo.db "BEGIN TRANSACTION;UPDATE STAINFO set ASSOCTIME=\"$_assoctime\", IPADDR=\"$_ip\", RSSI=\"$_rssi\" WHERE MAC=\"$mac\";COMMIT;" &
+sqlite3 /tmp/stationinfo.db "BEGIN TRANSACTION;UPDATE STAINFO set CHAN=\"$_chan\", ASSOCTIME=\"$_assoctime\", IPADDR=\"$_ip\", RSSI=\"$_rssi\" WHERE MAC=\"$mac\";COMMIT;" &
