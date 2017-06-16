abde5b6fed72e02c1442f527f2de75f5ac7a7d49
Leilei Wang <leilei.wang@oakridge.io>
2017-06-16 16:28:08 +0800

    add clientevent into supervisor

--- ok_base-files/lib/okos/supervisor.sh
+++ ok_base-files/lib/okos/supervisor.sh
@@ -3,13 +3,19 @@
 while :
 do
     sleep 30
-    [ -z "`pgrep capwapc`" ] && {
-        logger -p 5 "CAPWAP is exist abnormally, restart it !!!"
+    [ -z "`pgrep -f capwapc`" ] && {
+        logger -t supervisor -p 5 "CAPWAP is exit abnormally, restart it !!!"
         /etc/init.d/capwapc restart
     }
-    [ -z "`pgrep wifidog`" ] && {
-        logger -p 5 "WIFIDog is exist abnormally, restart it !!!"
+    [ -z "`pgrep -f wifidog`" ] && {
+        logger -t supervisor -p 5 "WIFIDog is exit abnormally, restart it !!!"
         /etc/init.d/wifidog restart
+    }
+    [ -z "`pgrep -f clientevent.py`" ] && {
+        logger -t supervisor -p 5 "CLIENTEVENT is exit abnormally, restart it !!!"
+        killall -9 clientevent.py
+        rm -rf /tmp/wifievent.pipe
+        /lib/okos/clientevent.py
     }
 done
 
