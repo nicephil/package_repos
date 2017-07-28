50ef02caae8a1809a6e40932c0c7b427e871f729
Leilei Wang <leilei.wang@oakridge.io>
2017-07-28 12:59:48 +0800

    generate .app image for ubnt

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-origin_file="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin"
-bin_file="bin_ap152_16M.app"
+swversion=$1
+origin_files__bin_files="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin|bin_ap152_16M.app openwrt-ar71xx-generic-ap147-16M-squashfs-sysupgrade.bin|bin_ap147_16M.app openwrt-ar71xx-generic-ubnt-unifi-lite-squashfs-sysupgrade.bin|bin_ubnt_unifi_lite.app openwrt-ar71xx-generic-ubnt-unifi-pro-squashfs-sysupgrade.bin|bin_ubnt_unifi_pro.app"
 
+
+
+
+for origin_file__bin_file in ${origin_files__bin_files}
+do
+
+OIFS=$IFS;IFS="|"; set -- ${origin_file__bin_file};origin_file=$1;bin_file=$2;IFS=$OIFS
+echo "${origin_file} --- ${bin_file}"
 tar czf ${origin_file}.tar.gz ${origin_file}
 
 # 64bytes header
@@ -12,8 +20,8 @@ header="\x27\x05\x19\x56\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
 echo -e "${header}" > ${bin_file}
 cat ${origin_file}.tar.gz >> ${bin_file}
 
-[ -n "$1" ] && {
-    mv ${bin_file} "$1"_${bin_file}
-    mv ${origin_file}.tar.gz "$1"_${origin_file}.tar.gz
+[ -n "$swversion" ] && {
+    mv ${bin_file} "$swversion"_${bin_file}
+    mv ${origin_file}.tar.gz "$swversion"_${origin_file}.tar.gz
 }
-
+done
