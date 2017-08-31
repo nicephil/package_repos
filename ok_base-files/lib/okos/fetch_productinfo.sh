03ee9827024af1cfb5637ed1a944fcf9e6f2725b
Leilei Wang <leilei.wang@oakridge.io>
2017-08-31 15:19:42 +0800

    formate serial number

--- ok_base-files/lib/okos/fetch_productinfo.sh
+++ ok_base-files/lib/okos/fetch_productinfo.sh
@@ -8,9 +8,10 @@ case "$board" in
 ubnt-erx)
     mac=$(hexdump -e '1/1 "%x:"' -n6 /dev/mtd2)
     mac=${mac%:*}
+    serial=`echo $mac|tr -d :`
     echo -e "config productinfo productinfo"
     echo -e  "\toption production ubnterx"
-    echo -e "\toption serial ${mac}"
+    echo -e "\toption serial ${serial}"
     echo -e "\toption mac ${mac}"
     ;;
 *)
