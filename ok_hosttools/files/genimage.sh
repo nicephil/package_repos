b7c11bfd27857a4119ce258ba770955311f56b65
Leilei Wang <leilei.wang@oakridge.io>
2017-09-02 19:33:35 +0800

    enable upload image to image.oakridge.vip
    Conflicts:
    	ok_hosttools/files/genimage.sh

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 swversion=$1
-origin_files__bin_files="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin|bin_ap152_16M.app openwrt-ar71xx-generic-ubnt-unifi-squashfs-sysupgrade.bin|bin_ubnt_unifi.app lede-ramips-mt7621-ubnt-erx-squashfs-sysupgrade.tar|bin_ubnt_erx.app"
-
+origin_files__bin_files="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin|bin_ap152_16M.app|ap152 openwrt-ar71xx-generic-ubnt-unifi-squashfs-sysupgrade.bin|bin_ubnt_unifi.app|ubntunifi lede-ramips-mt7621-ubnt-erx-squashfs-sysupgrade.bin|bin_ubnt_erx.app|ubnterx"
+server=image.oakridge.vip
 
 
 
 for origin_file__bin_file in ${origin_files__bin_files}
 do
 
-OIFS=$IFS;IFS="|"; set -- ${origin_file__bin_file};origin_file=$1;bin_file=$2;IFS=$OIFS
+OIFS=$IFS;IFS="|"; set -- ${origin_file__bin_file};origin_file=$1;bin_file=$2;rdir=$3;IFS=$OIFS
 echo "${origin_file} --- ${bin_file}"
 tar czf ${origin_file}.tar.gz ${origin_file}
 
@@ -21,7 +21,12 @@ echo -e "${header}" > ${bin_file}
 cat ${origin_file}.tar.gz >> ${bin_file}
 
 [ -n "$swversion" ] && {
-    mv ${bin_file} "$swversion"_${bin_file}
-    mv ${origin_file}.tar.gz "$swversion"_${origin_file}.tar.gz
+    mv ${bin_file} ${swversion}_${bin_file}
+    mv ${origin_file}.tar.gz ${swversion}_${origin_file}.tar.gz
+    # upload
+    scp ${swversion}_${bin_file} ${swversion}_${origin_file}.tar.gz image@${server}:/var/www/html/images/ap/${rdir}/.
+    ssh image@${server} "cd /var/www/html/images/ap/${rdir};
+    unlink latest-bin.app;ln -s ${swversion}_${bin_file} latest-bin.app;
+    unlink latest-sysupgrade.bin.tar.gz; ln -s ${swversion}_${origin_file}.tar.gz latest-sysupgrade.bin.tar.gz"
 }
 done
