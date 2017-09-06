b39d7a52ebac2faf0bbe31ce42187c827a479f2e
Chenyu Li <chenyu.li@oakridge.io>
2017-09-06 13:57:53 +0800

    Help you prepare a gateway

--- ok_base-files/lib/okos/go2gw.sh
+++ ok_base-files/lib/okos/go2gw.sh
@@ -7,13 +7,33 @@
 #
 ################################################################################
 
-export PATH="/lib/okos:$PATH"
+init ()
+{
+    opkg update
+    opkg install ip tcpdump
+}
 
-opkg update
-opkg install ip tcpdump
+reboot ()
+{
+    /etc/init.d/firewall stop
+    /etc/init.d/firewall disable
+    /etc/init.d/apfw stop
+    /etc/init.d/apfw disable
+    /etc/init.d/whitelist stop
+    /etc/init.d/whitelist disable
 
-/etc/init.d/firewall stop
-/etc/init.d/firewall disable
+    iptables -t nat -A POSTROUTING -o eth0.4090 -j MASQUERADE
+}
 
-iptables -t nat -A POSTROUTING -o eth0.4090 -j MASQUERADE
-
+case "$1" in
+    init)
+        init
+        reboot
+        ;;
+    reboot)
+        reboot
+        ;;
+    *)
+        echo "Usage: $0 [init|cleanup]" && exit 1
+        ;;
+esac
