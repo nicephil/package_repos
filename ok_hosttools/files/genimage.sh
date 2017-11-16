982dcc94a00007091b83cb4d4b8f5a2e42bc1dcf
Leilei Wang <leilei.wang@oakridge.io>
2017-11-16 21:56:48 +0800

    adjust build script

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 swversion=$1
-origin_files__bin_files="lede-ramips-mt7621-ubnt-erx-squashfs-sysupgrade.bin|bin_ubnt_erx.app|ubnterx"
+origin_files__bin_files="vmlinux-initramfs.elf|bin_ubnt_erx.gz|ubnterx"
 server=image.oakridge.vip
 
 
@@ -10,23 +10,13 @@ do
 
 OIFS=$IFS;IFS="|"; set -- ${origin_file__bin_file};origin_file=$1;bin_file=$2;rdir=$3;IFS=$OIFS
 echo "${origin_file} --- ${bin_file}"
-tar czf ${origin_file}.tar.gz ${origin_file}
-
-# 64bytes header
-header="\x27\x05\x19\x56\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
-\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
-\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
-\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30"
-echo -e "${header}" > ${bin_file}
-cat ${origin_file}.tar.gz >> ${bin_file}
+gzip -c "${origin_file}" > "${bin_file}"
 
 [ -n "$swversion" ] && {
     mv ${bin_file} ${swversion}_${bin_file}
-    mv ${origin_file}.tar.gz ${swversion}_${origin_file}.tar.gz
     # upload
-    scp ${swversion}_${bin_file} ${swversion}_${origin_file}.tar.gz image@${server}:/var/www/html/images/ap/${rdir}/.
-    ssh image@${server} "cd /var/www/html/images/ap/${rdir};
-    unlink latest-bin.app;ln -s ${swversion}_${bin_file} latest-bin.app;
-    unlink latest-sysupgrade.bin.tar.gz; ln -s ${swversion}_${origin_file}.tar.gz latest-sysupgrade.bin.tar.gz"
+    scp ${swversion}_${bin_file} image@${server}:/var/www/html/images/ap/${rdir}/okos/.
+    ssh image@${server} "cd /var/www/html/images/ap/${rdir}/okos;
+    unlink latest-bin.gz;ln -s ${swversion}_${bin_file} latest-bin.gz;"
 }
 done
