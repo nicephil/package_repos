834428b0ff17d7634629f3b4d5c292e8e26296c7
Leilei Wang <leilei.wang@oakridge.io>
2017-12-22 21:45:34 +0800

    adjust miwifi3 network

--- ok_base-files/bin/zzz.sh
+++
@@ -1,2 +0,0 @@
-scp llwang@192.168.254.191:/home/llwang/repos/master_for_lede-17.01/osdk_repos/bin/targets/ramips/mt7621/lede-ramips-mt7621-ubnt-erx-squashfs-sysupgrade.tar /tmp
-sysupgrade /tmp/lede-ramips-mt7621-ubnt-erx-squashfs-sysupgrade.tar
