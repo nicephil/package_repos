c6014cce1a7357434615fb1033e5003f83477b02
Leilei Wang <leilei.wang@oakridge.io>
2018-01-30 11:43:54 +0800

    add guestnetwork

--- ok_base-files/lib/okos/restartservices.sh
+++ ok_base-files/lib/okos/restartservices.sh
@@ -5,6 +5,5 @@ sync;echo 3 > /proc/sys/vm/drop_caches
 sleep 15
 /etc/init.d/wifidog restart&
 /etc/init.d/qos restart&
-iptables -F client_wan_uplink_traf&
-iptables -F client_wan_downlink_traf&
 /etc/init.d/arpwatch restart&
+/lib/okos/apfw.dyn restart&
