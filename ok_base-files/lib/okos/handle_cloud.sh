cbba927b6ed2d19489d78d62d9c25809be6218d3
Leilei Wang <leilei.wang@oakridge.io>
2018-02-02 17:49:39 +0800

    add overlay mount

--- ok_base-files/lib/okos/handle_cloud.sh
+++ ok_base-files/lib/okos/handle_cloud.sh
@@ -26,35 +26,68 @@ KEY="$(echo -n "${SALT}${_mac}" | md5sum | awk '{print $1}')"
 PORT="80"
 ADDR="api.oakridge.io"
 OAKMGR_PUB=""
+FIRSTBOOT="1"
+
+# 5. setup the new oakmgr
+setup_capwapc ()
+{
+    echo "====> set new oakmgr: $OAKMGR_PUB" | logger -t 'handle_cloud'
+    uci set capwapc.server.mas_server="$OAKMGR_PUB"; uci commit capwapc;
+    /etc/init.d/capwapc restart
+
+    while [ -z $(grep "$OAKMGR_PUB" /etc/capwapc/config.wtp 2>/dev/null) ]
+    do
+        sleep 3
+        echo "+++> set new oakmgr: $OAKMGR_PUB" | logger -t 'handle_cloud'
+        uci set capwapc.server.mas_server="$OAKMGR_PUB"; uci commit capwapc;
+        /etc/init.d/capwapc restart
+    done
+}
 
 while :
 do
     URL="http://${ADDR}:${PORT}/redirector/v1/device/register/?key=${KEY}"
-    echo curl -vv -m 10 -s -X POST -H "Content-type: application/json" -H "charset: utf-8" -H "Accept: */*" -d "$json_data" $URL |  logger -t 'handle_cloud'
-    response=$(curl -m 10 -s -X POST -H "Content-type: application/json" -H "charset: utf-8" -H "Accept: */*" -d "$json_data" $URL 2>/dev/ttyS0)
+    echo "aa>" "$json_data" $URL |  logger -t 'handle_cloud'
+    response=$(curl -q -m 10 -s -X POST -H "Content-type: application/json" -H "charset: utf-8" -H "Accept: */*" -d "$json_data" $URL 2>/dev/ttyS0)
     echo "----->$response" | logger -t 'handle_cloud'
 
     if [ -z "$response" ]
     then
         echo "response is null" | logger -t 'handle_cloud'
-        sleep 10
+        sleep 5
         continue
     fi
 
+    json_init
     json_load "$response"
     json_get_var _device "device"
     json_get_var _oakmgr_pub_name "oakmgr_pub_name"
     json_get_var _oakmgr_pub_port "oakmgr_pub_port"
 
-    if [ -n "$_oakmgr_pub_name" ]
+    if [ -z "$_oakmgr_pub_name" ]
+    then
+        echo "no valid oakmgr_pub_name, so query agian" | logger -t 'handle_cloud'
+        sleep 5
+        continue
+    elif [ "$_oakmgr_pub_name" = "$(uci get capwapc.server.mas_server 2>/dev/null)" ]
+    then
+        echo "existing oakmgr_pub_name:$_oakmgr_pub_name is the same as quried, so no action" | logger -t 'handle_cloud'
+        sleep 120
+        continue
+    fi
+
+    if [ "$FIRSTBOOT" = "1" ]
     then
         OAKMGR_PUB="$_oakmgr_pub_name"
-        break
+        echo "setup capwapc during firstboot" | logger -t 'handle_cloud'
+        setup_capwapc
+        FIRSTBOOT="0"
+    else
+        echo "_oakmgr_pub_name:$_oakmgr_pub_name, capwapc.mas_server=$(uci get capwapc.server.mas_server 2>/dev/null)" | logger -t 'handle_cloud'
+        sleep 5
+        reboot -f
     fi
 
 done
 
-# 5. setup the new oakmgr
-echo "====> set new oakmgr: $OAKMGR_PUB" | logger -t 'handle_cloud'
-uci set capwapc.server.mas_server="$OAKMGR_PUB"; uci commit capwapc;
-/etc/init.d/capwapc restart
+
