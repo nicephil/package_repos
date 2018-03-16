701b0fcd8b8332b5ed51acaf3b4157fc48f7bd83
Leilei Wang <leilei.wang@oakridge.io>
2018-03-16 16:09:50 +0800

    add latest-swversion.txt for ugprade tool

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -12,6 +12,7 @@ OIFS=$IFS;IFS="|"; set -- ${origin_file__bin_file};origin_file=$1;bin_file=$2;rd
 echo "${origin_file} --- ${bin_file}"
 echo "$swversion" > swversion.txt
 tar czf ${origin_file}.tar.gz ${origin_file} swversion.txt
+echo "$swversion" > latest-swversion.txt
 
 # 64bytes header
 header="\x27\x05\x19\x56\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\x30\
@@ -27,11 +28,11 @@ cat ${origin_file}.tar.gz >> ${bin_file}
     md5sum ${swversion}_${bin_file} | awk '{print $1}' > ${swversion}_${bin_file}.md5sum
     md5sum ${swversion}_${origin_file}.tar.gz | awk '{print $1}' > ${swversion}_${origin_file}.tar.gz.md5sum
     # upload
-    scp ${swversion}_${bin_file} ${swversion}_${origin_file}.tar.gz ${swversion}_${bin_file}.md5sum ${swversion}_${origin_file}.tar.gz.md5sum image@${server}:/var/www/html/images/ap/${rdir}/sysloader/.
+    scp latest-swversion.txt ${swversion}_${bin_file} ${swversion}_${origin_file}.tar.gz ${swversion}_${bin_file}.md5sum ${swversion}_${origin_file}.tar.gz.md5sum image@${server}:/var/www/html/images/ap/${rdir}/sysloader/.
     ssh image@${server} "cd /var/www/html/images/ap/${rdir}/sysloader;
-    unlink latest-bin.app;ln -s ${swversion}_${bin_file} latest-bin.app;
-    unlink latest-sysupgrade.bin.tar.gz; ln -s ${swversion}_${origin_file}.tar.gz latest-sysupgrade.bin.tar.gz;
-    unlink latest-bin.app.md5sum;ln -s ${swversion}_${bin_file}.md5sum latest-bin.app.md5sum;
-    unlink latest-sysupgrade.bin.tar.gz.md5sum; ln -s ${swversion}_${origin_file}.tar.gz.md5sum latest-sysupgrade.bin.tar.gz.md5sum;"
+    ln -sf ${swversion}_${bin_file} latest-bin.app;
+    ln -sf ${swversion}_${origin_file}.tar.gz latest-sysupgrade.bin.tar.gz;
+    ln -sf ${swversion}_${bin_file}.md5sum latest-bin.app.md5sum;
+    ln -sf ${swversion}_${origin_file}.tar.gz.md5sum latest-sysupgrade.bin.tar.gz.md5sum;"
 }
 done
