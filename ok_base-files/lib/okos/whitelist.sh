cf9a976429c45cc07bfd969be27776c458b5a50d
cyli <chenyu.li@oakridge.io>
2018-05-24 15:37:23 +0800

    change client access control on whitelist to fix CWP bugs when deploying PAK.

--- ok_base-files/lib/okos/whitelist.sh
+++ ok_base-files/lib/okos/whitelist.sh
@@ -1,68 +1,49 @@
 #!/bin/sh
 
 CMD="iptables"
-CHAIN="WhiteList"
+CHAIN_CC="ClientControl"
+CHAIN_WL="WhiteList"
+CHAIN_TP="GotoPortal"
+CHAINS="$CHAIN_CC $CHAIN_WL $CHAIN_TP"
 
-start ()
+add_to_whitelist ()
 {
+    local _mac_=$1
     for t in filter nat; do
-        $CMD -t $t -S $CHAIN > /dev/null 2>&1
+        $CMD -t $t -S $CHAIN_WL | grep -i $_mac_
         if [ $? -eq 0 ]; then
-            echo "$CHAIN in table $t is already exist."
+            echo "Client has been already in WhiteList on table $t ."
         else
-            echo "Create chain ${CHAIN} on table ${t}."
-            $CMD -t $t -N $CHAIN
-        fi
-
-        local basic_chain="FORWARD"
-        if [ $t != "filter" ]; then
-            basic_chain="PREROUTING"
-        fi
-
-        $CMD -t $t -S ${basic_chain} | grep $CHAIN
-        if [ $? -eq 0 ]; then
-            echo "${CHAIN} has been already added into table ${t}. No action."
-        else
-            local pos=1
-            if [ $t == "filter" ]; then
-                local basic_rule=`$CMD -t $t -L ${basic_chain} --line-number | grep "RELATED" | grep "ESTABLISHED" | grep "ACCEPT"`
-                if [ ! -z "${basic_rule}" -a $? -eq 0 ]; then
-                    echo "Find basic rule in talbe ${t}, attach WhiteList after it."
-                    i=`echo $basic_rule | awk '{print $1}'`
-                    pos=$(( i + 1 ))
-                    #pos=2
-                fi
-            fi
-
-            echo "Add WhiteList to position $pos on table ${t}."
-            $CMD -t $t -I ${basic_chain} $pos -j $CHAIN
+            echo "Add new client $_mac_ to WhiteList on table $t ."
+            $CMD -t $t -A $CHAIN_WL -m mac --mac-source $_mac_ -j ACCEPT
         fi
     done
 }
 
-stop ()
+add_to_gotoportal ()
 {
-    for t in filter nat; do
-        local basic_chain="FORWARD"
-        if [ $t != "filter" ]; then
-            basic_chain="PREROUTING"
+    local _mac_=$1
+    #for t in filter nat; do
+    for t in nat; do
+        $CMD -t $t -S $CHAIN_TP | grep -i $_mac_
+        if [ $? -eq 0 ]; then
+            echo "Client has been already directed to GotoPortal on table $t ."
+        else
+            echo "Add new client $_mac_ to GotoPortal on table $t ."
+            $CMD -t $t -A $CHAIN_TP -m mac --mac-source $_mac_ -j Portal
         fi
-        while [ true ]; do
-            $CMD -t $t -S ${basic_chain} | grep $CHAIN
-            if [ $? -eq 0 ]; then
-                echo "Delete the rule jumping to $CHAIN ."
-                $CMD -t $t -D ${basic_chain} -j $CHAIN
-            else
-                break
-            fi
-        done
+    done
+}
 
+del_from_whitelist ()
+{
+    local _mac_=$1
+    for t in filter nat; do
         while [ true ]; do
-            $CMD -t $t -S $CHAIN > /dev/null 2>&1
+            $CMD -t $t -S $CHAIN_WL | grep -i $_mac_ > /dev/null 2>&1
             if [ $? -eq 0 ]; then
-                echo "Flush chain $CHAIN and delete it."
-                $CMD -t $t -F $CHAIN
-                $CMD -t $t -X $CHAIN
+                echo "Delete client $_mac_ for WhiteList on table $t ."
+                $CMD -t $t -D $CHAIN_WL -m mac --mac-source $_mac_ -j ACCEPT
             else
                 break
             fi
@@ -70,35 +51,16 @@ stop ()
     done
 }
 
-add ()
+del_from_gotoportal ()
 {
     local _mac_=$1
-    for t in filter nat; do
-        $CMD -t $t -S $CHAIN > /dev/null 2>&1
-        if [ $? -ne 0 ]; then
-            echo "Chain $CHAIN has never been installed. Create it now."
-            start
-        fi
-
-        $CMD -t $t -S $CHAIN | grep -i $_mac_
-        if [ $? -eq 0 ]; then
-            echo "Client has been already exist."
-        else
-            echo "Add new client ${_mac_} ."
-            $CMD -t $t -A $CHAIN -m mac --mac-source $_mac_ -j ACCEPT
-        fi
-    done
-}
-
-del ()
-{
-    local _mac_=$1
-    for t in filter nat; do
+    #for t in filter nat; do
+    for t in nat; do
         while [ true ]; do
-            $CMD -t $t -S $CHAIN | grep -i $_mac_ > /dev/null 2>&1
+            $CMD -t $t -S $CHAIN_TP | grep -i $_mac_ > /dev/null 2>&1
             if [ $? -eq 0 ]; then
-                echo "Delete client $_mac_ ."
-                $CMD -t $t -D $CHAIN -m mac --mac-source $_mac_ -j ACCEPT
+                echo "Delete client $_mac_ from GotoPortal on table $t ."
+                $CMD -t $t -D $CHAIN_TP -m mac --mac-source $_mac_ -j Portal
             else
                 break
             fi
@@ -109,7 +71,9 @@ del ()
 show ()
 {
     for t in filter nat; do
-        $CMD -t $t -S $CHAIN
+        for chain in $CHAINS; do
+            $CMD -t $t -S $chain
+        done
     done
 }
 
