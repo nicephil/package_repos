235143a47041c05d981c3827089f06e95bdfaf43
Leilei Wang <leilei.wang@oakridge.io>
2018-07-20 17:39:25 +0800

    image build process

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 swversion=$1
-origin_files__bin_files="lede-x86-64-ramfs.bzImage|bin_x86_64_gw.gz|x86_64_gw"
+origin_files__bin_files="lede-x86-64-ramfs.bzImage|bin_x86_64_gw.bzImage|x86_64_gw"
 server=image.oakridge.vip
 
 function add_new_release() 
@@ -29,7 +29,7 @@ do
 
     OIFS=$IFS;IFS="|"; set -- ${origin_file__bin_file};origin_file=$1;bin_file=$2;rdir=$3;IFS=$OIFS
     echo "${origin_file} --- ${bin_file}"
-    gzip -c "${origin_file}" > "${bin_file}"
+    cp "${origin_file}" "${bin_file}"
 
     [ -n "$swversion" ] && {
         mv ${bin_file} ${swversion}_${bin_file}
@@ -37,8 +37,8 @@ do
         # upload
         scp ${swversion}_${bin_file} ${swversion}_${bin_file}.md5sum image@${server}:/var/www/html/images/${rdir}/okos/.
         ssh image@${server} "cd /var/www/html/images/${rdir}/okos;
-        ln -sf ${swversion}_${bin_file} latest-okos.gz;
-        ln -sf ${swversion}_${bin_file}.md5sum latest-okos.gz.md5sum"
+        ln -sf ${swversion}_${bin_file} latest-okos.bzImage;
+        ln -sf ${swversion}_${bin_file}.md5sum latest-okos.bzImage.md5sum"
     }
 
     if [ $rdir = "x86_64_gw" ]
