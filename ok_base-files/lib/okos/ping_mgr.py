83ef1deee31b7cd2e557bc73becc9ac70501b41a
Leilei Wang <leilei.wang@oakridge.io>
2018-08-07 22:44:26 +0800

    import python-ping to do icmp latency detect

--- ok_base-files/lib/okos/ping_mgr.py
+++ ok_base-files/lib/okos/ping_mgr.py
@@ -5,7 +5,7 @@ import okos_utils
 from okos_utils import log_debug, log_info, log_warning, log_err, log_crit
 import json
 from constant import const
-import ubus
+import ping
 
 class PingMgr(threading.Thread):
 
@@ -21,7 +21,5 @@ class PingMgr(threading.Thread):
     def process_data(self):
         while self.running:
             for site in self.sites:
-                param = {'site':site}
-                result = ubus.call('ping_mgr', 'status', param)
-                print result
+                loss, mtt, att = ping.quiet_ping(site)
             time.sleep(5)
