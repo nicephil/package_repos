be9583321a79cdbfac6c0f56b39d224e828c43b0
Leilei Wang <leilei.wang@oakridge.io>
2018-08-09 18:30:40 +0800

    reduce wifi ioctl access

--- ok_base-files/lib/okos/runtimefixup.sh
+++ ok_base-files/lib/okos/runtimefixup.sh
@@ -30,13 +30,18 @@ trap 'runtimefixup_trap; exit 1' INT TERM ABRT QUIT ALRM
 
 touch /tmp/runtimefixup.lock
 
-all_db=$(sqlite3 /tmp/stationinfo.db 'select * from stainfo';sqlite3 /tmp/statsinfo.db 'select * from statsinfo' | awk -F'|' '!a[$1]++')
+all_db=$(sqlite3 /tmp/stationinfo.db 'select * from stainfo' 2>/dev/null;sqlite3 /tmp/statsinfo.db 'select * from statsinfo' | awk -F'|' '!a[$1]++' 2>/dev/null)
+apstats_log=$(apstats -a -R 2>/dev/null)
+all_clients=$(for ath in `iwconfig 2>/dev/null | awk '/ath/{print $1}'`;do wlanconfig $ath list sta; done | awk '$1 !~ /ADDR/{if (!(a[$1]++)) print $1}')
+all_stationinfo_db=$(sqlite3 /tmp/stationinfo.db "SELECT * FROM STAINFO" 2>/dev/null)
+all_iptables_log=$(iptables -S WhiteList 2>/dev/null;iptables -t nat -S GotoPortal 2>/dev/null)
+
 
 # delete client who already gone
 for client_tmp in $all_db
 do
     OIFS=$IFS;IFS='|';set -- $client_tmp;_mac=$1;_ath=$2;IFS=$OIFS        
-    __ath=$(apstats -s -m $_mac 2>/dev/null | awk '/'"$_mac"'/{print substr($7,1, length($7)-1);exit}')
+    __ath=$(echo "$apstats_log" | awk '/'"$_mac"'/{print substr($7,1, length($7)-1);exit}')
     # no find mac really
     if [ -z "$__ath" ]
     then
@@ -44,7 +49,7 @@ do
         /lib/okos/wifievent.sh $_ath AP-STA-DISCONNECTED $_mac ""
 		continue
     fi
-    __iptables_check=`(iptables -S WhiteList;iptables -t nat -S GotoPortal)| grep -i $_mac`
+    __iptables_check=`echo "$all_iptables_log" | grep -i $_mac`
     if [ -z "$__iptables_check" ]
     then
         runtimefixup_err_log "missed _ath:$__ath client:$_mac connected event"
@@ -54,17 +59,16 @@ do
 done
 
 # active missed client
-for client in $(for ath in `iwconfig 2>/dev/null | awk '/ath/{print $1}'`;do wlanconfig $ath list sta; done | awk '$1 !~ /ADDR/{if (!(a[$1]++)) print $1}')
+for client in $all_clients
 do
     (
     unset _mac
     unset _ath
-    client_tmp=$(sqlite3 /tmp/stationinfo.db "SELECT * FROM STAINFO WHERE MAC = \"$client\"")
+    client_tmp=$(echo "$all_stationinfo_db" | awk '/'"$client"'/{print $0;exit}')
     [ -n "$client_tmp" ] && {
         OIFS=$IFS;IFS='|';set -- $client_tmp;_mac=$1;_ath=$2;IFS=$OIFS        
     }
-
-    __ath=$(apstats -s -m $client 2>/dev/null | awk '/'"$client"'/{print substr($7,1, length($7)-1);exit}')
+    __ath=$(echo "$apstats_log" | awk '/'"$client"'/{print substr($7,1, length($7)-1);exit}')
     # no find mac really
     if [ -z "$__ath" ]
     then
