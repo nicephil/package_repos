86a0d5b63d5ef6314f9d2484d7869b5947f97852
Leilei Wang <leilei.wang@oakridge.io>
2018-09-17 14:08:06 +0800

    remove seprate python-ubus and put it into ubus

--- python-ubus/CMakeFiles/python.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "build"
-  "CMakeFiles/python"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/python.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
