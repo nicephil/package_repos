ed04ed01f60c10586411e0c3714c7a8940153a45
Leilei Wang <leilei.wang@oakridge.io>
2018-11-09 10:41:06 +0800

    adjust kernel param, and reduce priority of predownload

--- ok_base-files/lib/okos/3_cleanup_clientevent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-clientevent=1 crontab=1 predownload=1 syslog=1 sh /lib/okos/cleanup.sh
-
--- ok_base-files/lib/okos/4_cleanup_arpwatch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-arpwatch=1 crontab=1 predownload=1 syslog=1 sh /lib/okos/cleanup.sh
-
