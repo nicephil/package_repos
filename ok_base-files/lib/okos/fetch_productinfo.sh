d2005416c2b17e23e137a507e3d19bd03f82a03c
Leilei Wang <leilei.wang@oakridge.io>
2018-11-12 12:55:16 +0800

    add dependency in luci Makefile

--- ok_base-files/lib/okos/fetch_productinfo.sh
+++ ok_base-files/lib/okos/fetch_productinfo.sh
@@ -17,7 +17,7 @@ cpu="$(cat /proc/cpuinfo | awk -F':' '/model name/{print $2;exit}' | awk '{print
 mem="$(cat /proc/meminfo | awk '/MemTotal/{a=$2/1024/1024;if(a != int(a)) a=a+1;print int(a)"G";exit}')"
 echo -e "config productinfo productinfo"
 echo -e  "\toption production OKGW"
-echo -e  "\toption model Oakridge_OKGW"
+echo -e  "\toption model MTK_OKGW"
 echo -e "\toption serial ${serial}"
 echo -e "\toption mac ${mac}"
 echo -e "\toption swversion `cat /etc/issue`"
