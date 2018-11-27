553d5fa24d8bd4e6fe9187ff1b14ca0e93950113
Leilei Wang <leilei.wang@oakridge.io>
2018-11-27 16:50:37 +0800

    fix atf clear exception and config timeout if config gets error

--- ok_base-files/lib/okos/qos.sh
+++ ok_base-files/lib/okos/qos.sh
@@ -246,12 +246,12 @@ qos_start ()
 
 qos_atf_deinit()
 {
-    iwpriv "ath50" commitatf 0 > /dev/null 2>&1
-    iwpriv "ath60" commitatf 0 > /dev/null 2>&1
     wlanconfig "ath50" delatfgroup public > /dev/null 2>&1
     wlanconfig "ath50" delatfgroup private > /dev/null 2>&1
     wlanconfig "ath60" delatfgroup public > /dev/null 2>&1
     wlanconfig "ath60" delatfgroup private > /dev/null 2>&1
+    #iwpriv "ath50" commitatf 0 > /dev/null 2>&1
+    #iwpriv "ath60" commitatf 0 > /dev/null 2>&1
 }
 
 
