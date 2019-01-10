c7c2085e193bc1aad779b6a04ceab519ab4ad138
Leilei Wang <leilei.wang@oakridge.io>
2019-01-10 14:40:34 +0800

    add failure log if image upload failed

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -2,9 +2,9 @@
 swversion=$1
 origin_files__bin_files="openwrt-ar71xx-generic-ap152-16M-squashfs-sysupgrade.bin|bin.app|ap152 openwrt-ar71xx-generic-ubnt-unifi-squashfs-sysupgrade.bin|bin.app|ubntunifi"
 server="image.oakridge.vip"
-#server=$(host -W 5 $server 2>/dev/null | awk '{if(!match($4,"found:"))print $4;exit}')
-#[ -z "$server" ] && server="106.14.245.228"
-server="106.14.245.228"
+server=$(host -W 5 $server 2>/dev/null | awk '{if(!match($4,"found:"))print $4;exit}')
+[ -z "$server" ] && server="106.14.245.228"
+#server="106.14.245.228"
 
 
 
@@ -32,6 +32,10 @@ cat ${origin_file}.tar.gz >> ${bin_file}
     md5sum ${swversion}_${origin_file}.tar.gz | awk '{print $1}' > ${swversion}_${origin_file}.tar.gz.md5sum
     # upload
     scp latest-swversion.txt ${swversion}_${bin_file} ${swversion}_${origin_file}.tar.gz ${swversion}_${bin_file}.md5sum ${swversion}_${origin_file}.tar.gz.md5sum image@${server}:/var/www/html/images/ap/${rdir}/sysloader/.
+    [ "$?" != "0" ] && {
+        echo "XXXXXXXXXXXXXXXX upload image failed!!! XXXXXXXXXX"
+        exit 1
+    }
     ssh image@${server} "cd /var/www/html/images/ap/${rdir}/sysloader;
     ln -sf ${swversion}_${bin_file} latest-bin.app;
     ln -sf ${swversion}_${origin_file}.tar.gz latest-sysupgrade.bin.tar.gz;
