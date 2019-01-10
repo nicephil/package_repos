efeac6ba67cc00c53e0f5ea0767d8cfaab5555f4
Leilei Wang <leilei.wang@oakridge.io>
2019-01-10 13:57:49 +0800

    add log if upload image failed

--- ok_hosttools/files/genimage.sh
+++ ok_hosttools/files/genimage.sh
@@ -4,7 +4,6 @@ origin_files__bin_files="openwrt-ar71xx-generic-vmlinux-initramfs.elf|vmlinux_ap
 server="image.oakridge.vip"
 server=$(host -W 5 $server 2>/dev/null | awk '{if(!match($4,"found:"))print $4;exit}')
 [ -z "$server" ] && server="106.14.245.228"
-server="106.16.254.228"
 
 function add_new_release() 
 {
@@ -39,6 +38,10 @@ do
         md5sum -b ${swversion}_${bin_file} | awk '{print $1}' > ${swversion}_${bin_file}.md5sum
         # upload
         scp ${swversion}_${bin_file} ${swversion}_${bin_file}.md5sum image@${server}:/var/www/html/images/ap/${rdir}/okos/.
+        [ "$?" != "0" ] && {
+            echo "XXXXXXXXXXXXXXXX upload image failed!!! XXXXXXXXXX"
+            exit 1
+        }
         ssh image@${server} "cd /var/www/html/images/ap/${rdir}/okos;
         ln -sf ${swversion}_${bin_file} latest-okos.gz;
         ln -sf ${swversion}_${bin_file}.md5sum latest-okos.gz.md5sum"
