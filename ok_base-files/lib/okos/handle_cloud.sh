4db64705bded6dae2379f8b86bef424a46ec56d1
Leilei Wang <leilei.wang@oakridge.io>
2019-01-14 11:55:28 +0800

    add reboot if version is different from SDC binding version

--- ok_base-files/lib/okos/handle_cloud.sh
+++ ok_base-files/lib/okos/handle_cloud.sh
@@ -6,6 +6,14 @@
 . /lib/ar71xx.sh
 . /lib/functions/network.sh
 
+rand()
+{
+    min=$1
+    max=$(($2-$min+1))
+    num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
+    echo $(($num%$max+$min))
+}
+
 # 2. fetch device info
 config_load "productinfo"
 config_get _mac "productinfo" "mac"
@@ -26,7 +34,15 @@ echo "===>$json_data" | logger -t 'handle_cloud'
 SALT="Nobody knows"
 KEY="$(echo -n "${SALT}${_mac}" | md5sum | awk '{print $1}')"
 PORT="80"
-ADDR="api.oakridge.io"
+DEFAULT_ADDR="api.oakridge.io"
+SAVED_ADDR=$(uci get capwapc.image.oakmgr_pub_name 2>/dev/null)
+if [ -z "$SAVED_ADDR" -o "$SAVED_ADDR" = "0.0.0.0"  ]
+then
+    ADDR="$DEFAULT_ADDR"
+else
+    ADDR="$SAVED_ADDR"
+fi
+ADDR="$DEFAULT_ADDR"
 OAKMGR_PUB=""
 GW_OK="1"
 CAPWAP_FAILURE_COUNT=0
@@ -44,7 +60,7 @@ do
     [ "$GW_OK" = "0" -a -f "/tmp/firstboot_report" ]  && {
         echo "----->looks gateway is unreachable, renew ip now" | logger -t 'handle_cloud'
         #ubus call network.interface notify_proto '{"action": 2, "interface": "lan1", "signal": 16}'
-        /etc/init.d/network restart
+        #/etc/init.d/network restart
         sleep 20
         GW_OK="1"
     }
@@ -72,32 +88,48 @@ do
 
     if [ -z "$response" ]
     then
-        echo "response is null" | logger -t 'handle_cloud'
+        echo "redirector/SDC response is null" | logger -t 'handle_cloud'
         sleep 5
         continue
     fi
 
     json_init
     json_load "$response"
+    json_get_var _okos_md5sum "okos_md5sum"
+    json_get_var _image_url "image_url"
+    json_get_var _boot_delay "boot_delay"
     json_get_var _device "device"
     json_get_var _oakmgr_pub_name "oakmgr_pub_name"
     json_get_var _oakmgr_pub_port "oakmgr_pub_port"
 
-    if [ -z "$_oakmgr_pub_name" ]
+    # get queried version
+    _image_file=${_image_url##*/}
+    _ver_var=${_image_file%%_*}
+
+    echo "_oakmgr_pub_name:$_oakmgr_pub_name, capwapc.mas_server=$(uci get capwapc.server.mas_server 2>/dev/null), quried_version:$_ver_var, local_version:$(cat /etc/issue)" | logger -t 'handle_cloud'
+
+    if [ -z "$_oakmgr_pub_name" -o -z "$_ver_var" ]
     then
         echo "no valid oakmgr_pub_name, so query agian" | logger -p user.info -t 'handle_cloud'
+        if [ -n "$_oakmgr_pub_name" ] 
+        then
+            ADDR="$_oakmgr_pub_name"
+        else
+            ADDR="$DEFAULT_ADDR"
+        fi
         sleep 5
         continue
-    elif [ "$_oakmgr_pub_name" = "$(uci get capwapc.server.mas_server 2>/dev/null)" ]
+    elif [ "$_oakmgr_pub_name" = "$(uci get capwapc.server.mas_server 2>/dev/null)" -a "$_ver_var" = "$(cat /etc/issue)" ]
     then
-        echo "existing oakmgr_pub_name:$_oakmgr_pub_name is the same as quried, so no action" | logger -p user.info -t 'handle_cloud'
+        echo "existing oakmgr_pub_name:$_oakmgr_pub_name _ver_var:$_ver_var is the same as quried, so no action" | logger -p user.info -t 'handle_cloud'
         sleep 120
         continue
     fi
 
-    echo "_oakmgr_pub_name:$_oakmgr_pub_name, capwapc.mas_server=$(uci get capwapc.server.mas_server 2>/dev/null)" | logger -t 'handle_cloud'
-    echo "reboot by handle_cloud, as oakmgr changed!!" | logger -p user.info -t '01-SYSTEM-LOG'
-    sleep 60
+    timeout=$(rand 1 1200)
+    echo "will be reboot by handle_cloud after ${timeout}s, as oakmgr/oakos version changed!!" | logger -p user.info -t '01-SYSTEM-LOG'
+    sleep 30
+    sleep $timeout
     reboot -f
 done
 
